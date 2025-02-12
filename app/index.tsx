9570b9be4ca35c1eebdfaab4df34b24f2b4fd5e3
Migueel00 <rojassoriamiguelangel95@gmail.com>
2025-02-12 22:55:07 +0100

    [GYM-1][ADD]SplashScreen, UPDATE proyect structure

--- app/index.tsx
+++ app/index.tsx
@@ -1,33 +1,51 @@
 import { Text, View } from "react-native";
 import Menu from "./components/Menu";
 import { Player } from "./components/Menu";
-import useAppStore from "./store/appStore";
-import { useEffect } from "react";
+import useAppStore from "@/src/store/appStore";
+import { useEffect, useState, useCallback } from "react";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function Index() {
-  const { user } = useAppStore();
+	const [appIsReady, setAppIsReady] = useState<boolean>(false);
+	const { user } = useAppStore();
 
-  useEffect(() => {
-    console.log("user : " + user);
-  }, [user])
+	useEffect(() => {
+		console.log("user : " + user);
+	}, [user])
 
-  const player : Player = {
-    id: 1,
-    nickname: 'PlayerOne'
-  }
+	const player: Player = {
+		id: 1,
+		nickname: 'PlayerOne'
+	}
 
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-  
 
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Menu player={player}></Menu>
-    </View>
-  );
+	useEffect(() => {
+		setAppIsReady(true);
+	}, []);
+
+	const onLayoutRootView = useCallback(async () => {
+		if (appIsReady) {
+			await SplashScreen.hideAsync();
+		}
+	}, [appIsReady]);
+
+	if (!appIsReady) {
+		return null;
+	}
+
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+			onLayout={onLayoutRootView}
+		>
+			<Text>Edit app/index.tsx to edit this screen.</Text>
+			<Menu player={player}></Menu>
+		</View>
+	);
 }
--- src/__test__/helpers/sum.test.ts
+++ src/__test__/helpers/sum.test.ts
@@ -1,4 +1,4 @@
-import { sum } from "@/app/helpers/helpers";
+import { sum } from "@/src/helpers/helpers";
 
 describe('test the sum function', () => {
   it('should calculate correctly the sum of 2 number and round floor the result', () => {
